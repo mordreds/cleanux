e4e2c249e69f0d02ad09c8f97a7358007f07c552
Osborne Mordreds <osborne.mordred@gmail.com>
2018-08-13 11:31:32 +0000

    inhouse stat re-arranged

--- application/modules/inhouse/views/inhouse.php
+++ application/modules/inhouse/views/inhouse.php
@@ -5,19 +5,6 @@
       <div class="col-sm-6 col-md-2">
         <div class="panel panel-body panel-body-accent">
           <div class="media no-margin">
-            <div class="media-left media-middle">
-              <i class="icon-watch2 icon-3x text-warning-400"></i>
-            </div>
-            <div class="media-body text-right">
-              <h3 class="no-margin text-semibold"><?=number_format(@$overdue_orders)?></h3>
-              <span class="text-uppercase text-size-mini text-muted">OverDue Orders </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-2">
-        <div class="panel panel-body panel-body-accent">
-          <div class="media no-margin">
             <div class="media-left media-middle" style="padding-right: 0px">
               <i class="icon-basket icon-3x text-success-400"></i>
             </div>
@@ -37,6 +24,19 @@
             <div class="media-body text-right">
               <h3 class="no-margin text-semibold"><?=number_format($processing_orders)?></h3>
               <span class="text-uppercase text-size-mini text-muted">Processing Orders</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-2">
+        <div class="panel panel-body panel-body-accent">
+          <div class="media no-margin">
+            <div class="media-left media-middle">
+              <i class="icon-watch2 icon-3x text-warning-400"></i>
+            </div>
+            <div class="media-body text-right">
+              <h3 class="no-margin text-semibold"><?=number_format(@$overdue_orders)?></h3>
+              <span class="text-uppercase text-size-mini text-muted">OverDue Orders </span>
             </div>
           </div>
         </div>
